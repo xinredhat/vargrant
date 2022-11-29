c4b6180299e61b6b6b9182bf223b3ad5aac6c58c
Xin Jiang <xjiang@redhat.com>
2022-11-29 14:12:01 +0800

    added oc binary

--- fedora/sh/install_softwares.sh
+++ fedora/sh/install_softwares.sh
@@ -7,3 +7,7 @@ sudo chmod +x /usr/local/bin/yq
 
 sudo wget "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -O /usr/local/bin/kubectl
 sudo chmod +x /usr/local/bin/kubectl
+
+wget -O "/tmp/ocp-client.tar.gz"  https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.11.9/openshift-client-linux.tar.gz
+tar -C "/tmp" -xzf "/tmp/ocp-client.tar.gz"  oc
+sudo mv /tmp/oc /usr/local/bin/oc
